b0f4c056476cb4ec9cfad5252b52f1ace8e33bf6
Yunuuuu <yunyunpp96@outlook.com>
2025-04-10 09:54:25 +0800

    remove `series_lapply`

--- R/standalone-polars.R
+++ R/standalone-polars.R
@@ -18,6 +18,9 @@
 # function
 
 # ## Changelog
+# 2025-04-10
+# - Remove `series_lapply`, please use other package like mirai instead
+#
 # 2025-02-26:
 # - Add `install_polars`
 # - Add `use_polars`
@@ -54,70 +57,6 @@ use_polars <- function(reason) {
             ns = pkg_namespace()
         )
     }
-}
-
-#' @param .x A [Series][polars::Series] object.
-#' @param .fn Must call `$collect_in_background()` method.
-#' @param ... Additional arguments passed to `.fn`.
-#' @param .progress Additional arguments passed to [cli::cli_progress_bar].
-#' @keywords internal
-#' @noRd
-series_lapply <- function(.x, .fn, ..., .progress, .threads = 1L) {
-    # .threads <- min(as.integer(.threads), pl$thread_pool_size())
-    poos_size <- max(.threads, 1L)
-    n <- .x$len()
-    if (is_installed("cli")) {
-        bar <- rlang::inject(
-            getExportedValue("cli", "cli_progress_bar")(
-                !!!.progress, total = n, clear = FALSE
-            )
-        )
-    }
-    out <- vector("list", n)
-    # pools: store the index of result
-    # NA means this pool can be used
-    pools <- rep_len(NA_integer_, poos_size)
-    i <- pool <- 1L
-    while (i <= n || !all(is.na(pools))) {
-        handle_index <- .subset(pools, pool)
-        # if i > n, we skip add task
-        if (i <= n && is.na(handle_index)) {
-            # this pool can be used, we add task into this pool
-            # `.fn()` must return with `$collect_in_background()` method
-            # For Series, cannot subset with `[[`
-            out[[i]] <- .fn(.x$slice(i - 1L, 1L), ...)
-            pools[pool] <- i
-            i <- i + 1L
-        }
-
-        # if there is a task in this pool
-        # we check if we can release this pool
-        if (!is.na(handle_index)) {
-            polars_handle <- .subset2(out, handle_index)
-            if (polars_handle$is_finished()) {
-                # collect result from this pool and release this pool
-                out[[handle_index]] <- polars_handle$join()
-                pools[pool] <- NA_integer_
-                if (is_installed("cli")) {
-                    getExportedValue("cli", "cli_progress_update")(
-                        inc = 1L, id = bar
-                    )
-                }
-
-                # this pool has been released, so we directly
-                # step into next cycle and re-use this pool
-                next
-            }
-        }
-
-        # search next pool
-        if (pool == poos_size) {
-            pool <- 1L
-        } else {
-            pool <- pool + 1L
-        }
-    }
-    out
 }
 
 # nocov end
